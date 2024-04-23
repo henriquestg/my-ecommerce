78b55c1234007ce47b9509722b0ef8561e6b0572
Henrique <rigoni1912@gmail.com>
2024-04-23 20:22:49 -0300

    refac: smooth scroll

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -5,6 +5,7 @@ import Logo from "../../../public/logo.png";
 import Image from "next/image";
 import { Market } from "../Icon/Markte";
 import { Notification } from "../Icon/Notification";
+import { Link } from "react-scroll";
 
 export function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -30,18 +31,19 @@ export function NavBar() {
           </div>
           <ul className={`nav-list ${menuOpen ? "open" : ""}`}>
             <li>
-              <a href="#" id="home">
-                Home
-              </a>
+              <Link to="products" smooth={true} duration={500}>
+                Products
+              </Link>
             </li>
             <li>
-              <a href="#products">Products</a>
+              <Link to="about" smooth={true} duration={500}>
+                About Us
+              </Link>
             </li>
             <li>
-              <a href="#about">About Us</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
+              <Link to="contact" smooth={true} duration={500}>
+                Contact
+              </Link>
             </li>
             <li>
               <input placeholder="Search" />
@@ -60,82 +62,3 @@ export function NavBar() {
     </nav>
   );
 }
-
-// "use client";
-// import Image from "next/image";
-// import { ButtonHeader } from "../ButtonHeader";
-// import { Market } from "../Icon/Markte";
-// import { Notification } from "../Icon/Notification";
-
-//
-
-// import "./styles.scss";
-// import { useState } from "react";
-
-// export function NavBar() {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setMenuOpen(!menuOpen);
-//   };
-//   return (
-//     <div className="container">
-//       <div className="navbar-container">
-//
-//         <div className="navbar-full">
-//           <div className="navbar">
-//             <div
-//               className={`menu-icon ${menuOpen ? "open" : ""}`}
-//               onClick={toggleMenu}
-//             >
-//               <div className="bar"></div>
-//               <div className="bar"></div>
-//               <div className="bar"></div>
-//             </div>
-//             <ul className={`nav-list ${menuOpen ? "open" : ""}`}>
-//               <li>
-//                 <a href="#" id="home">
-//                   Home
-//                 </a>
-//               </li>
-//               <li>
-//                 <a href="#mytech">Products</a>
-//               </li>
-//               <li>
-//                 <a href="#about">About Us</a>
-//               </li>
-//               <li>
-//                 <a href="#project">Contact</a>
-//               </li>
-//             </ul>
-//           </div>
-//           <ul>
-//             <li>
-//               <a href="#">Home</a>
-//             </li>
-//             <li>
-//               <a href="#">Products</a>
-//             </li>
-//             <li>
-//               <a href="#">About Us</a>
-//             </li>
-//             <li>
-//               <a href="#">Contact</a>
-//             </li>
-//           </ul>
-//           <input placeholder="Search" />
-//           <div className="marketplace">
-//             <button>
-//               <Market /> Card
-//             </button>
-//           </div>
-//           <div className="notification">
-//             <button>
-//               <Notification /> Notification
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
